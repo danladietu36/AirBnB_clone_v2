97c67d83a767aaa9943222ad1e1a4f1417008fda
Danladi Etu <danladietu36@gmail.com>
2023-04-18 05:59:48 -0700

    codefix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,8 +5,8 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- with the password : hbnb_dev_pwd if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 --grants all privileges to new user
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
---Grant sellect privilege for the user hbnb_dev in the de performance schema
+--Grant SELECT privilege for the user hbnb_dev in the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
