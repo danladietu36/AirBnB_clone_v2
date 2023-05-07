4cb2003109aaf0807d26f88cfeb59bb66889176c
Danladi Etu <danladietu36@gmail.com>
2023-05-07 13:02:22 -0700

    added

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,20 +2,17 @@
 from fabric.api import *
 import os
 
-
 env.hosts = ['34.239.248.111', '54.175.89.17']
 
 
 def do_clean(number=0):
-    """ Delete oudated archives.
+    """Deletes outdated archives
     Arguments:
-        number(int): no of archives to keep
-
-     If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives
+        number (int): no of archives
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
     """
-
-     number = 1 if int(number) == 0 else int(number)
+    number = 1 if int(number) == 0 else int(number)
 
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
