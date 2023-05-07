be42dd92554fe8ab2051caf61e7ef1721c41e573
Danladi Etu <danladietu36@gmail.com>
2023-05-07 14:54:45 -0700

    added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""" Module to compress web package
+"""Module to compress web package
 """
-
 from os import path
-from Fabric.api import *
 from datetime import datetime
+from fabric.api import *
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['34.239.248.111', '54.175.89.17']
 env.user = 'ubuntu'
 env.keyFilename = '~/.ssh/id_rsa'
 
