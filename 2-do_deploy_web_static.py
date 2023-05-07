61243ed77406a6dba2bf48cad176522d12b9e714
Danladi Etu <danladietu36@gmail.com>
2023-05-07 07:24:44 -0700

    added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,57 @@
 #!/usr/bin/python3
-"""Module to compress web static package
+""" Module to compress web package
 """
+
+from os import path
 from fabric.api import *
 from datetime import datetime
-from os import path
 
-env.hosts = ['34.239.248.111', '54.175.89.17']
+
+env.hosts = ['100.25.19.204', '54.157.159.85']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.keyFilename = '~/.ssh/id_rsa'
+
 
 def do_deploy(archive_path):
-        """Deploys  files to server
+        """Deploy web files to server
         """
         try:
                 if not (path.exists(archive_path)):
                         return False
 
-                # upload archive to servers
+                # upload archive
                 put(archive_path, '/tmp/')
 
-                # creates target direcory
+                # create target dir
                 timestamp = archive_path[-18:-4]
                 run('sudo mkdir -p /data/web_static/\
 releases/web_static_{}/'.format(timestamp))
 
-                # delete the .tgz file and uncomprees archive
+                # uncompress archive and delete .tgz
                 run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
 /data/web_static/releases/web_static_{}/'
                     .format(timestamp, timestamp))
 
-                # code to remove the archive
+                # remove archive
                 run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
-                # code to remove erroneous directory
-                 run('sudo rm -rf /data/web_static/releases/\
+                # move contents into host web_static
+                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+
+                # remove extraneous web_static dir
+                run('sudo rm -rf /data/web_static/releases/\
 web_static_{}/web_static'
                     .format(timestamp))
 
-                 # code to delete pre-existing symbilic link
-                  run('sudo rm -rf /data/web_static/current')
+                # delete pre-existing sym link
+                run('sudo rm -rf /data/web_static/current')
 
-                  # code to re-establish symbolic link
+                # re-establish symbolic link
                 run('sudo ln -s /data/web_static/releases/\
 web_static_{}/ /data/web_static/current'.format(timestamp))
         except:
                 return False
 
+        # return True on success
         return True
