e1ce229c72bb032c2377fddcd8c23764f1a3ecc5
Danladi Etu <danladietu36@gmail.com>
2023-05-20 15:50:10 -0700

    added

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,12 @@ from flask import Flask
 
 app = Flask("__name__")
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """ Function to display Hello HBNB!"""
     return ("Hello HBNB!")
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=500, debug=None)
