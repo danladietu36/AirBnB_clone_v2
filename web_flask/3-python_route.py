3ab971646f13c450887eb4d9f499408fd63e4188
Danladi Etu <danladietu36@gmail.com>
2023-05-21 15:09:13 -0700

    added

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,12 +27,11 @@ def c_text(text):
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is cool"):
     """ Function to display Python followed by the value of text variable"""
     return "Python {}".format(text.replace("_", " "))
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
