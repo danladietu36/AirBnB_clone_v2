4e2c4714a4464bacc7b41ff71b6ded97c46a8413
Danladi Etu <danladietu36@gmail.com>
2023-05-21 14:17:25 -0700

    added

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -25,7 +25,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def text():
     """Function to display C followed by text variable's value"""
-    return ("C {}".format(text.replace("_", " ")))
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
