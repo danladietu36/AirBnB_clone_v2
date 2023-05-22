159d0967e37921535f2079fc408f534266b88110
Danladi Etu <danladietu36@gmail.com>
2023-05-22 14:44:38 -0700

    added

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,10 +4,8 @@
    It must be listening at 0.0.0.0, port 5000
 """
 
-from flask import Flask
-
-
-app = Flask("__name__")
+from flask import Flask, render_template
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
