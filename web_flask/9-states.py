79cdfe85a1949f49e5af7a642ff9a989a3ce2e26
Danladi Etu <danladietu36@gmail.com>
2023-05-23 15:03:01 -0700

    added

---
+++ web_flask/9-states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" A script that start a Flask web application
+   It must be listening at 0.0.0.0., port 5000
+"""
+from flask import Flask
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close():
+    """ A method to close the SQLAlchemy session"""
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def state():
+    """ A method to display html page with state."""
+    states = storage.all(State)
+    return render_template("9-states.html", states=states, mode='all')
+
+
+@app.route("/states/<id>", strict_slashes)
+def stateId(id):
+    """ A method to display html page with state and scities"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template("9-states.html", states=states, mode=id)
+        return render_template("9-states.html", states=states, mode=None)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0.0", port=5000)
